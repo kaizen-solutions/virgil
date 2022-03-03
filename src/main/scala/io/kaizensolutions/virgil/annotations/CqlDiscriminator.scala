dcce9fe40a1ce8a263b69de76973dfc51c6e8ee1
calvinlfer <calvin.l.fer@gmail.com>
2022-03-02 20:46:33 -0500

    Fix 2.12.x incompatibilities

---
+++ src/main/scala/io/kaizensolutions/virgil/annotations/CqlDiscriminator.scala
@@ -0,0 +1,9 @@
+package io.kaizensolutions.virgil.annotations
+
+import scala.annotation.StaticAnnotation
+
+case class CqlDiscriminator(name: String) extends StaticAnnotation
+object CqlDiscriminator {
+  @inline def extract(annotations: Seq[Any]): Option[String] =
+    annotations.collectFirst { case CqlDiscriminator(name) => name }
+}
